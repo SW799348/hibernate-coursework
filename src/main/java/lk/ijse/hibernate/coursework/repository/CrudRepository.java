4ef21f208393d15f1982365e89af6547064bac95
sw799348 <sw799348@gmail.com>
2024-03-04 19:41:29 +0530

    login form created

--- src/main/java/lk/ijse/hibernate/coursework/repository/CrudRepository.java
+++ src/main/java/lk/ijse/hibernate/coursework/repository/CrudRepository.java
@@ -1,4 +1,12 @@
 package lk.ijse.hibernate.coursework.repository;
 
-public interface CrudRepository {
+public interface CrudRepository<T,ID> extends SuperRepository {
+
+    ID save(T object);
+
+    void update(T object);
+
+    T get(ID id);
+
+    void delete(T object);
 }
