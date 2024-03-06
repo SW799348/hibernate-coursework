4c127fb61dd0539d16dfcb109c3f6a07afed2d56
sw799348 <sw799348@gmail.com>
2024-03-06 20:01:28 +0530

    ui's created

---
+++ src/main/java/lk/ijse/hibernate/coursework/controller/AdminHomeController.java
@@ -0,0 +1,30 @@
+package lk.ijse.hibernate.coursework.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class AdminHomeController {
+
+    @FXML
+    private JFXButton btnBook;
+
+    @FXML
+    private JFXButton btnBranch;
+
+    @FXML
+    private JFXButton btnHome;
+
+    @FXML
+    private JFXButton btnLogout;
+
+    @FXML
+    private JFXButton btnUser;
+
+    @FXML
+    private Pane mainPane;
+
+    @FXML
+    private Pane sidePane;
+
+}
--- src/main/java/lk/ijse/hibernate/coursework/controller/AdminLoginFormController.java
+++ src/main/java/lk/ijse/hibernate/coursework/controller/AdminLoginFormController.java
@@ -1,4 +1,20 @@
 package lk.ijse.hibernate.coursework.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class AdminLoginFormController {
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
 }
